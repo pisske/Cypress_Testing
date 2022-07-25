6b5135710e5f88b18db0d8d62581b709ff597e15
Vladimir Pisacic <vladimir.pisacic@qualityhouse.rs>
2022-07-25 11:30:12 +0200

    Finished with fixture

--- cypress/integration/automation-test-store/contact_us.js
+++ cypress/integration/automation-test-store/contact_us.js
@@ -1,29 +1,30 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via Automation Test Store", () => {
-  it.only("Should be able to submit a successful submission via contact us form", () => {
+  before(function () {
+    cy.fixture("userDetails").as("user");
+  });
+  it("Should be able to submit a successful submission via contact us form", () => {
     cy.visit("https://www.automationteststore.com/");
-    // cy.get("#contact-us ").click({ force: true });
-    //cy.get(".info_links_footer > :nth-child(5) > a").click();
-    //cy.xpath("a[contains(@href,'contact')]").click();
     cy.get("a[href$='contact']")
       .click()
-      .then(function (itemHeaderText) {
-        cy.log("Selected the followig items " + itemHeaderText.text());
+      .then(function (linkText) {
+        cy.log("Clicked on link using text: " + linkText.text());
       });
-    cy.get("#ContactUsFrm_first_name").type("Vladimir");
-    cy.get("#ContactUsFrm_email").type("vladimir@gmail.com");
-    // cy.get("#ContactUsFrm_email")
-    //   .should("have.attr", "name", "email")
-    cy.get("#ContactUsFrm_enquiry").type("some random text ");
-    //cy.get(".col-md-6 > .btn").click();
+    //cy.xpath("//a[contains(@href, 'contact')]").click();
+    cy.get("@user").then((user) => {
+      cy.get("#ContactUsFrm_first_name").type(user.first_name);
+      cy.get("#ContactUsFrm_email").type(user.email);
+    });
+    cy.get("#ContactUsFrm_email").should("have.attr", "name", "email");
+    cy.get("#ContactUsFrm_enquiry").type(
+      "Do you provide additional discount on bulk orders?"
+    );
     cy.get("button[title='Submit']").click();
     cy.get(".mb40 > :nth-child(3)").should(
       "have.text",
       "Your enquiry has been successfully sent to the store owner!"
     );
-    cy.log("test has completed");
+    cy.log("Test has completed!");
   });
-
-  it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {});
 });
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
+  before(function () {
+    cy.fixture("example").then(function (data) {
+      //this.data= data
+      globalThis.data = data;
+    });
+  });
+
   it("Should be able to submit a successful submission via contact us form", () => {
     // cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
     cy.visit("http://www.webdriveruniversity.com");
@@ -9,9 +16,9 @@ describe("Test Contact Us form via WebdriverUni", () => {
     cy.title().should("include", "WebDriver | Contact Us");
     cy.url().should("include", "contactus");
     // cy.get("#contact-us ").click({ force: true });
-    cy.get('[name="first_name"]').type("Vladimir");
-    cy.get('[name="last_name"]').type("Jovic");
-    cy.get('[name="email"]').type("vladimir@gmail.com");
+    cy.get('[name="first_name"]').type(data.last_name);
+    cy.get('[name="last_name"]').type(data.last_name);
+    cy.get('[name="email"]').type(data.email);
     cy.get("textarea.feedback-input").type("lakjflkjaslkdfj;lasdjf");
     cy.get('[type="submit"]').click();
     cy.get("h1").should("have.text", "Thank You for your Message!");
